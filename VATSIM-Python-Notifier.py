210ad4b0868690e6c0ce8ef811bead4931d4f9eb
Aaron Albertson <aaron.albertson@gmail.com>
2020-03-07 10:20:07 -0500

    Adjusted filter, and added new messaging

--- VATSIM-Python-Notifier.py
+++ VATSIM-Python-Notifier.py
@@ -3,6 +3,7 @@ k_servers = 'kafka-datafeed.vatsim.net:9092'
 messagefilter = ['KROA_ATIS', 'KROA_APP']
 
 import json
+from datetime import datetime
 from json import loads
 from kafka import KafkaConsumer
 from kafka.errors import BrokerNotAvailableError, NoBrokersAvailable
@@ -20,19 +21,35 @@ for message in consumer:
     message = message.value
     data = message['data']
     if message['message_type'] == 'add_client':
-        prettyprint = json.dumps(message, indent=4, separators=(',',':'))
-        print(prettyprint)
+        # prettyprint = json.dumps(message, indent=4, separators=(',',':'))
+        # print(prettyprint)
         if data['callsign'] == 'ROA_DEL':
-            print("**** I see it!!")
+            timestamp = str(datetime.now())
+            member = data['member']
+            cid = str(member['cid'])
+            name = member['name']
+            callsign = data['callsign']
+
+            # prettyprint = json.dumps(message, indent=4, separators=(',',':'))
+            # print(prettyprint)
+            # print(data)
+            print("[" + timestamp + "] - " + name  + " (" + cid + ") has opened " + callsign + " on VATSIM.")
         else:
             pass
     else:
         pass
     if message['message_type'] == 'remove_client':
-        prettyprint = json.dumps(message, indent=4, separators=(',',':'))
-        print(prettyprint)
         if data['callsign'] == 'ROA_DEL':
-            print("**** I see it!!")
+            timestamp = str(datetime.now())
+            member = data['member']
+            cid = str(member['cid'])
+            name = member['name']
+            callsign = data['callsign']
+
+            prettyprint = json.dumps(message, indent=4, separators=(',',':'))
+            print(prettyprint)
+            # print(data)
+            print("[" + timestamp + "] - " + name  + " (" + cid + ") has closed " + callsign + " on VATSIM.")
         else:
             pass
     else:
