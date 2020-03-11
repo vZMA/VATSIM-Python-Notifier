9e5a8fa4bcff8b196b60b55d1948148f5290ba40
Aaron <aaron.albertson@gmail.com>
2020-03-11 14:01:50 -0400

    Update VATSIM-Python-Notifier.py

--- VATSIM-Python-Notifier.py
+++ VATSIM-Python-Notifier.py
@@ -31,6 +31,7 @@ messagefilter = ['DCA_','IAD_','BWI_','PCT_','ADW_','DC_C','RIC_','ROA_','ORF_',
 def discord_webhook(callsign, name, cid, rating_long, server, status):
 
     webhook = DiscordWebhook(url=webhookurl)
+    timestamp = str(datetime.now())
 
     if status == "online":
         embed = DiscordEmbed(title=callsign + " - Online", description=callsign + ' is now online on the VATSIM network.', color=65290)
@@ -106,6 +107,7 @@ def vatsim_notifier():
     print("[" + timestamp + "] - Notifer Started!")
     # Evaluate results for callsign sign in and outs. FOR LOOP
     for message in consumer:
+        timestamp = str(datetime.now())
         message = message.value
         data = message['data']
         if message['message_type'] == 'add_client':
