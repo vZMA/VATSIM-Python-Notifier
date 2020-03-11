ce4f0b5b6c2318ff42f71bff3a8ecd3e4c515947
Aaron <aaron.albertson@gmail.com>
2020-03-11 13:59:37 -0400

    Update VATSIM-Python-Notifier.py
    removed ZDC_ from filter to take out OBS from showing up.

--- VATSIM-Python-Notifier.py
+++ VATSIM-Python-Notifier.py
@@ -24,7 +24,7 @@ global rating_long
 messagefilter = ['DCA_','IAD_','BWI_','PCT_','ADW_','DC_C','RIC_','ROA_','ORF_','ACY_','NGU_',
                 'NTU_','NHK_','RDU_','CHO_','HGR_','LYH_','EWN_','LWB_','ISO_','MTN_','HEF_',
                 'MRB_','PHF_','SBY_','NUI_','FAY_','ILM_','NKT_','NCA_','NYG_','DAA_','DOV_',
-                'POB_','GSB_','WAL_','CVN_','ZDC_','DC_0','DC_1','DC_2','DC_3','DC_5','DC_N',
+                'POB_','GSB_','WAL_','CVN_','DC_0','DC_1','DC_2','DC_3','DC_5','DC_N',
                 'DC_S','DC_E','DC_W','DC_I']
 # Defs
 
